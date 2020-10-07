2cb11e845a6dd307aaa2e9d88304a82776735440
Albert <ratajczakalbert@gmail.com>
2020-10-07 18:33:38 +0200

    adding rsi_cross_signals

--- analysis/rsi.py
+++ analysis/rsi.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def rsi(prices, alpha=1/14):
+def rsi(prices: pd.DataFrame, alpha: float=1/14):
     """
     Clculates Relative Strength Index (RSI).
 
@@ -22,8 +22,35 @@ def rsi(prices, alpha=1/14):
 
     output_rsi = -100 / (rs+1)
     output_rsi = output_rsi + 100
-    
+
     output_rsi = output_rsi.rename('RSI')
     output_rsi = output_rsi.to_frame()
 
     return output_rsi
+
+
+def rsi_cross_signals(rsi_values: pd.DataFrame, cross_line: float, direction: str='onrise'):
+    """
+    Calculates buy/sell signals for given RSI signal line.
+
+    :rsi_values: pandas.DataFrame with RSI column
+    :cross_line: signal threshold line, when signal line crosses this line signal is set
+    :direction: direction the signal line should cross threshold line 
+    ('onrise' - signal increasing, 'onfall' - signal decreasing)
+    :return: pandas.Series with with True values for days when signal appears
+    """
+    if not (0 < cross_line < 100):
+        raise ValueError('cross_line takes values from 0 to 100')
+
+    rsi_copy = pd.DataFrame()
+    rsi_copy['RSI'] = rsi_values['RSI']
+    rsi_copy['RSI day before'] = rsi_values['RSI'].shift(1)
+
+    if direction is 'onrise':
+        output = (rsi_copy['RSI'] >= cross_line ) & (rsi_copy['RSI day before'] < cross_line)
+    elif direction is 'onfall':
+        output = (rsi_copy['RSI'] <= cross_line ) & (rsi_copy['RSI day before'] > cross_line)
+    else:
+        raise ValueError('wrong value for direction, must be "onrise" or "onfall"')
+
+    return output
