6517fa79823695243b1fe838165aab4c8a2762e6
Albert <ratajczakalbert@gmail.com>
2020-11-27 17:24:35 +0100

    up

--- main.py
+++ main.py
@@ -17,13 +17,15 @@ sns.set()
 START_DATE = '2014-01-01'
 END_DATE = '2020-10-31'
 TRAIDING_DAYS = Stock('WIG').ohlc[START_DATE:END_DATE].index
+
 MY_WALLET = Wallet(commission_rate=0.0038, min_commission=3.0)
 MY_WALLET.money = 12000
+
 MAX_POSITIONS = 5
 TAKE_PROFIT = 0.9
 STOP_LOSS = 0.025
-MOVING_STOP_LOSS = 0.055
 ACTIVE_TRAIDING = False
+
 TRADED_TICKERS = wig20_2019
 TRADED_TICKERS.update(mwig40)
 # ==============================================================================
