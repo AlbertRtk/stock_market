97c2a703302398af26d211f287f1165bc994b3d3
Albert <ratajczakalbert@gmail.com>
2021-01-01 19:33:16 +0100

    working on EMA strategy

--- stock_market_insights/strategies/ema_strategy.py
+++ stock_market_insights/strategies/ema_strategy.py
@@ -5,15 +5,60 @@
 from marketools.analysis import ema
 import math
 
-EMA_LONG_PERIOD = 50
+EMA_LONG_PERIOD = 100
 EMA_MID_PERIOD = 15
 EMA_SHORT_PERIOD = 5
-PRICE_OSCILLATIONS_MIN_PERIOD = 21
-TAKE_PROFIT = 0.9
+MIN_LONG_EMA_TREND_LENGTH = 2
+TAKE_PROFIT = 0.5
 STOP_LOSS = 0.025
 MAX_POSITIONS = 5
-MIN_INVESTMENT = 1000
+MIN_INVESTMENT = 3000
 MAX_INVESTMENT = 1000000
+
+
+def set_ema_long_period(value):
+    global EMA_LONG_PERIOD
+    EMA_LONG_PERIOD = value
+
+
+def set_ema_mid_period(value):
+    global EMA_MID_PERIOD
+    EMA_MID_PERIOD = value
+
+
+def set_ema_short_period(value):
+    global EMA_SHORT_PERIOD
+    EMA_SHORT_PERIOD = value
+
+
+def set_min_long_ema_trend_length(value):
+    global MIN_LONG_EMA_TREND_LENGTH
+    MIN_LONG_EMA_TREND_LENGTH = value
+
+
+def set_take_profit(value):
+    global TAKE_PROFIT
+    TAKE_PROFIT = value
+
+
+def set_stop_loss(value):
+    global STOP_LOSS
+    STOP_LOSS = value
+
+
+def set_max_positions(value):
+    global MAX_POSITIONS
+    MAX_POSITIONS = value
+
+
+def set_min_investment(value):
+    global MIN_INVESTMENT
+    MIN_INVESTMENT = value
+
+
+def set_max_investment(value):
+    global MAX_INVESTMENT
+    MAX_INVESTMENT = value
 
 
 def ema_strategy(day, wallet, traded_stocks, *args, **kwargs):
@@ -30,22 +75,11 @@ def ema_strategy(day, wallet, traded_stocks, *args, **kwargs):
             ema_mid = ema(ohlc=tck_ohlc, window=EMA_MID_PERIOD)
             ema_short = ema(ohlc=tck_ohlc, window=EMA_SHORT_PERIOD)
 
-            short_gt_long = ema_short > ema_long
-            short_gt_mid = ema_short > ema_mid
-            mid_gt_long = ema_mid > ema_long
-
-            recent_price_oscillations = short_gt_long.tail(PRICE_OSCILLATIONS_MIN_PERIOD+1)
-            recent_price_oscillations = recent_price_oscillations.head(PRICE_OSCILLATIONS_MIN_PERIOD)
-            no_price_oscillations = not any(recent_price_oscillations)
-
             # buy signals
-            #   - short crosses long from bottom, or
-            buy = short_gt_long[-1] and not short_gt_long[-2]
-            # buy = buy and
-            #   - short crosses mid from bottom and mid greater than long
-            # TODO
+            buy = (ema_short[-1] > ema_mid[-1]) and (ema_short[-2] < ema_mid[-2])
+            buy = buy and all([ema_long[-i] > ema_long[-i-1] for i in range(1, MIN_LONG_EMA_TREND_LENGTH)])
 
-            if buy and no_price_oscillations:
+            if buy:
                 if not wallet.get_volume_of_stocks(tck):
                     # buy!
                     invest = wallet.total_value / MAX_POSITIONS
@@ -55,8 +89,8 @@ def ema_strategy(day, wallet, traded_stocks, *args, **kwargs):
                     volume_to_buy = math.floor(invest / close_price)
                     stocks_to_buy[tck] = (volume_to_buy, None)
 
-            # sell signals - short crosses mid from top
-            sell = short_gt_mid[-2] and not short_gt_mid[-1]
+            # sell signals
+            sell = ema_long[-1] < ema_long[-2]
 
             if sell and (tck in wallet.list_stocks()):
                 stocks_to_sell[tck] = (wallet.get_volume_of_stocks(tck), None)
