1fc22f973bacec2d6c5bb642748dc844faf83a81
Albert <ratajczakalbert@gmail.com>
2021-01-02 18:07:04 +0100

    up

--- stock_market_insights/check_strategy_performance.py
+++ stock_market_insights/check_strategy_performance.py
@@ -1,16 +1,20 @@
 from simulator import Simulator
-from marketools import Stock, Wallet, store_data
-from stock_market_insights.stock_index import wig20_2019, mwig40
+from marketools import Stock, Wallet, store_data, StockQuotes
+from stock_index import wig20_2019, mwig40
 from tqdm import tqdm
 
 
-from stock_market_insights.strategies import ema_strategy as my_strategy
+from strategies import ema_strategy as my_strategy
+
+
+StockQuotes.check_for_update = False
+store_data()
 
 
 # === SIMULATOR CONFIG =========================================================
 START_DATE = '2015-01-01'  # '2015-01-01'
-END_DATE = '2019-12-31'  # '2019-12-31'
-TRADING_DAYS = Stock('WIG').ohlc[START_DATE:END_DATE].index
+END_DATE = '2020-12-31'  # '2019-12-31'
+TRADING_DAYS = Stock('CCC').ohlc[START_DATE:END_DATE].index
 MY_WALLET = Wallet(commission_rate=0.0038, min_commission=3.0)
 MY_WALLET.money = 10000
 TRADED_TICKERS = wig20_2019
@@ -19,9 +23,7 @@ TRADED_TICKERS.update(mwig40)
 
 
 if __name__ == '__main__':
-    store_data()
     print('Preparing data...')
-    wig = Stock('WIG')
     stocks_data = dict()
     for tck in tqdm(TRADED_TICKERS):
         stocks_data[tck] = Stock(tck)
@@ -29,5 +31,5 @@ if __name__ == '__main__':
 
     my_simulator = Simulator(TRADING_DAYS, stocks_data, MY_WALLET, show_plot=True)
 
-    result = my_simulator.run(my_strategy, wig=wig)
+    result = my_simulator.run(my_strategy)
     print('\n', result.tail(1))
--- stock_market_insights/optimize_strategy.py
+++ stock_market_insights/optimize_strategy.py
@@ -1,16 +1,20 @@
 from simulator import Simulator
-from marketools import Stock, Wallet, store_data
-from stock_market_insights.stock_index import wig20_2019, mwig40
+from marketools import Stock, Wallet, store_data, StockQuotes
+from stock_index import wig20_2019, mwig40
 from tqdm import tqdm
 
 
-from stock_market_insights.strategies import ema_strategy as my_strategy
-from stock_market_insights.strategies.ema_strategy import *
+from strategies import ema_strategy as my_strategy
+from strategies.ema_strategy import *
+
+
+StockQuotes.check_for_update = False
+store_data()
 
 # === SIMULATOR CONFIG =========================================================
 START_DATE = '2015-01-01'  # '2015-01-01'
 END_DATE = '2019-12-31'  # '2019-12-31'
-TRADING_DAYS = Stock('WIG').ohlc[START_DATE:END_DATE].index
+TRADING_DAYS = Stock('CCC').ohlc[START_DATE:END_DATE].index
 MY_WALLET = Wallet(commission_rate=0.0038, min_commission=3.0)
 MY_WALLET.money = 10000
 TRADED_TICKERS = wig20_2019
@@ -19,7 +23,6 @@ TRADED_TICKERS.update(mwig40)
 
 
 if __name__ == '__main__':
-    store_data()
     print('Preparing data...')
     wig = Stock('WIG')
     stocks_data = dict()
@@ -29,16 +32,16 @@ if __name__ == '__main__':
 
     my_simulator = Simulator(TRADING_DAYS, stocks_data, MY_WALLET, show_plot=False)
 
-    for i_mid in range(12, 15, 1):
-        set_ema_mid_period(i_mid)
+    for i_sl in range(10, 16, 1):
+        set_stop_loss(i_sl/1000)
 
-        for i_short in range(5, 8, 1):
-            set_ema_short_period(i_short)
+        for i_posn in range(3, 6, 1):
+            set_max_positions(i_posn)
 
             my_simulator.reset()
             result = my_simulator.run(my_strategy).tail(1)['Wallet state']
             result = float(result)
-            summary = f'180\t{i_mid}\t{i_short}\t{result}\n'
+            summary = f'{i_sl/1000}\t{i_posn}\t{result}\n'
 
             with open('strategy_optimization.txt', 'a') as f:
                 f.write(summary)
