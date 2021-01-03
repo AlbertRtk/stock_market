714c4871cd301aca9a85e5a11d74d90a42c737f5
Albert <ratajczakalbert@gmail.com>
2021-01-03 17:49:41 +0100

    up

--- stock_market_insights/strategies/ema_strategy.py
+++ stock_market_insights/strategies/ema_strategy.py
@@ -4,7 +4,6 @@
 
 from marketools.analysis import ema
 import math
-from scipy.stats import linregress
 
 
 EMA_LONG_PERIOD = 180
@@ -15,9 +14,6 @@ STOP_LOSS = 0.015
 MAX_POSITIONS = 3
 MIN_INVESTMENT = 1000
 MAX_INVESTMENT = 1000000
-
-EMA_LONG_TREND_PERIOD = 20
-MIN_EMA_LONG_RELATIVE_SLOPE = 0.001
 
 
 def ema_strategy(day, wallet, traded_stocks, *args, **kwargs):
@@ -34,13 +30,8 @@ def ema_strategy(day, wallet, traded_stocks, *args, **kwargs):
             ema_mid = ema(ohlc=tck_ohlc, window=EMA_MID_PERIOD)
             ema_short = ema(ohlc=tck_ohlc, window=EMA_SHORT_PERIOD)
 
-            slope, *_ = linregress(ema_long.values[-EMA_LONG_TREND_PERIOD:],
-                                   list(range(EMA_LONG_TREND_PERIOD)))
-            slope_rel = slope / ema_long[-EMA_LONG_TREND_PERIOD]
-
             # buy signals
             buy = (ema_short[-1] > ema_mid[-1] > ema_long[-1]) and (ema_mid[-2] > ema_short[-2] > ema_long[-2])
-            buy = buy and (slope_rel >= MIN_EMA_LONG_RELATIVE_SLOPE)
 
             if buy:
                 if not wallet.get_volume_of_stocks(tck):
@@ -107,16 +98,6 @@ def set_min_investment(value):
 def set_max_investment(value):
     global MAX_INVESTMENT
     MAX_INVESTMENT = value
-
-
-def set_ema_long_trend_period(value):
-    global EMA_LONG_TREND_PERIOD
-    EMA_LONG_TREND_PERIOD = value
-
-
-def set_min_ema_long_relative_slope(value):
-    global MIN_EMA_LONG_RELATIVE_SLOPE
-    MIN_EMA_LONG_RELATIVE_SLOPE = value
 
 
 if __name__ == '__main__':
