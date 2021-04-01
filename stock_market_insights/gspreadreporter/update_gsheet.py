f0d4fc1f6134d1df248e0fb2213fd0c8b3ddca25
Albert <ratajczakalbert@gmail.com>
2021-04-01 15:48:32 +0200

    move select_stocks_with_increased_volume

--- stock_market_insights/gspreadreporter/update_gsheet.py
+++ stock_market_insights/gspreadreporter/update_gsheet.py
@@ -5,39 +5,36 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from marketools import Stock, store_data
 from report_sheet import update_report
-from wallet_sheet import update_wallet
 from config import *
 
 
 store_data()
 
 
-def select_increased_volume_stocks_by_factor(stocks_dict: dict, long=90, factor=3.3) -> dict:
-    """TODO: This function should not be part of this file."""
+def select_stocks_with_increased_volume(stocks_dict: dict, long=90, factor=3.3) -> dict:
     selected = dict()
 
     for tck in stocks_dict.keys():
         stock = stocks_dict[tck]
         long_mean = stock.mean_volume(long)
-        volume = stock.volume
-        if (volume/long_mean) > factor:
+        if (stock.volume/long_mean) > factor:
             selected[tck] = stock
 
     return selected
 
 
 def update_stocks_spreadsheet(spreadsheet):
-    wallet = spreadsheet.worksheet(WS_WALLET)
+    # wallet = spreadsheet.worksheet(WS_WALLET)
     report = spreadsheet.worksheet(WS_REPORT)
     watched = spreadsheet.worksheet(WS_WATCHED)
 
     """ UPDATE WALLET WORKSHEET """
-    update_wallet(wallet)
+    # update_wallet(wallet)
 
     """ GET INCREASED VOLUME STOCKS """
     stocks_to_analyse = {s: Stock(s) for s in MY_TICKERS}
     print('\nSelecting stocks for report...')
-    selected_stocks = select_increased_volume_stocks_by_factor(stocks_to_analyse)
+    selected_stocks = select_stocks_with_increased_volume(stocks_to_analyse)
     print(f'{len(selected_stocks)} stocks selected')
 
     """ UPDATE ANALYSIS REPORT """
