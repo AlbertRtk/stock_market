a705e89f38e77ba578441f08950da1565a28db81
Albert <ratajczakalbert@gmail.com>
2021-04-01 10:04:20 +0200

    adding daily_report.py

---
+++ stock_market_insights/daily_report.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("marketools")
+
+from marketools import Stock, store_data
+from stock_index import wig20_2019, mwig40
+from datetime import date
+
+store_data()
+
+""" WIP """
+
