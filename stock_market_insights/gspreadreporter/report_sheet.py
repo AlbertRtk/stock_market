312b0d6fbce9abacd7b61eab5306e436c7e83b17
Albert <ratajczakalbert@gmail.com>
2021-04-01 16:55:37 +0200

    new gspread report - only increased volume stocks

--- stock_market_insights/gspreadreporter/report_sheet.py
+++ stock_market_insights/gspreadreporter/report_sheet.py
@@ -1,5 +1,33 @@
-from config import *
 from datetime import datetime
+from marketools.analysis import simple_relative_price_change
+
+
+ACEL_UPDATE_TIME = 'B1'
+
+ACOL_FIRST = 'A'
+ACOL_LAST = 'R'
+
+COL_TICKER = 0  # col A
+COL_PRICE = 1
+COL_PREVIOUS = 2
+COL_CHANGE = 3
+COL_CHANGE_PERC = 4
+COL_DAILY = 5
+COL_DAILY_PERC = 6
+COL_OPEN = 7
+COL_HIGH = 8
+COL_LOW = 9
+COL_VOL = 10
+COL_AVG_VOL_SHORT = 11
+COL_AVG_VOL_LONG = 12
+COL_PBV = 13
+COL_EPS = 14
+COL_PE = 15
+COL_DIV_YIELD = 16
+COL_PLOT = 17
+
+ROW_VOL_SELECTED_FIRST = 43
+ROW_LAST = 1000
 
 
 def clear_worksheet_range(worksheet, range_):
@@ -10,24 +38,28 @@ def clear_worksheet_range(worksheet, range_):
 
 
 def fill_in_report_row(row, share):
-    long_mean = share.mean_volume(90)
-    short_mean = share.mean_volume(5)
-    row[TICKERS_COL - 1].value = share.ticker
-    row[REPORT_PRICES_COL - 1].value = share.last_ohlc['Close']
-    row[REPORT_PBV_COL - 1].value = share.pbv
-    row[REPORT_EPS_COL - 1].value = share.eps
-    row[REPORT_PE_COL - 1].value = share.pe
-    row[REPORT_DIVIDEND_YIELD_COL - 1].value = share.dividend_yield
-    row[REPORT_VOLUME_MEAN_LONG - 1].value = round(long_mean, 0)
-    row[REPORT_VOLUME_MEAN_SHORT - 1].value = round(short_mean, 0)
-    row[REPORT_VOLUME - 1].value = share.volume
-    row[REPORT_PLOT_LINK_COL - 1].value = share.stooq_plot_link
-    row[REPORT_SHORT_LONG_RATIO - 1].value = round(short_mean/long_mean, 2)
-    row[REPORT_VOLUME_LONG_RATIO - 1].value = round(share.volume/long_mean, 2)
+    row[COL_TICKER].value = share.ticker
+    row[COL_PRICE].value = share.last_ohlc['Close']
+    row[COL_PREVIOUS].value = None
+    row[COL_CHANGE].value = None
+    row[COL_CHANGE_PERC].value = None
+    row[COL_DAILY].value = share.last_ohlc['Close'] - share.last_ohlc['Open']
+    row[COL_DAILY_PERC].value = simple_relative_price_change(share.last_ohlc['Close'], share.last_ohlc['Open'])
+    row[COL_OPEN].value = share.last_ohlc['Open']
+    row[COL_HIGH].value = share.last_ohlc['High']
+    row[COL_LOW].value = share.last_ohlc['Low']
+    row[COL_VOL].value = share.volume
+    row[COL_AVG_VOL_SHORT].value = round(share.mean_volume(5), 0)
+    row[COL_AVG_VOL_LONG].value = round(share.mean_volume(90), 0)
+    row[COL_PBV].value = share.pbv
+    row[COL_EPS].value = share.eps
+    row[COL_PE].value = share.pe
+    row[COL_DIV_YIELD].value = share.dividend_yield
+    row[COL_PLOT].value = share.stooq_plot_link
 
 
 def update_report_row(report_wks, row_idx, share):
-    cell_range = f'{REPORT_FIRST_ACOL}{row_idx}:{REPORT_LAST_ACOL}{row_idx}'
+    cell_range = f'{ACOL_FIRST}{row_idx}:{ACOL_LAST}{row_idx}'
     row = report_wks.range(cell_range)
     fill_in_report_row(row, share)
     report_wks.update_cells(row)
@@ -35,16 +67,14 @@ def update_report_row(report_wks, row_idx, share):
 
 def update_report(report_wks, stocks: dict):
     print(f'\nPreparing analysis report for {len(stocks)} stocks:')
-    str_today = datetime.now().strftime('%Y-%m-%d')
-    report_wks.update_acell(UPDATE_TIME_ACELL, str_today)
+    str_today = datetime.now().strftime('%Y-%m-%d, %H:%M')
+    report_wks.update_acell(ACEL_UPDATE_TIME, str_today)
 
     """ Clear worksheet from old data """
-    last_filled_row = len(report_wks.col_values(TICKERS_COL))
-    cell_range = f'{REPORT_FIRST_ACOL}{FIRST_DATA_ROW}:{REPORT_LAST_ACOL}{last_filled_row}'
+    cell_range = f'{ACOL_FIRST}{ROW_VOL_SELECTED_FIRST}:{ACOL_LAST}{ROW_LAST}'
     clear_worksheet_range(report_wks, cell_range)
 
     """ Update worksheet """
-    for i, tck in enumerate(stocks, FIRST_DATA_ROW):
+    for i, tck in enumerate(stocks, ROW_VOL_SELECTED_FIRST):
         share = stocks[tck]
-        # print(f'{1+i-FIRST_DATA_ROW}\t{tck}\t Data from: {share.date}')
         update_report_row(report_wks, i, share)
--- stock_market_insights/gspreadreporter/update_gsheet.py
+++ stock_market_insights/gspreadreporter/update_gsheet.py
@@ -4,32 +4,42 @@ sys.path.append("../marketools")
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from marketools import Stock, store_data
+from marketools.analysis import select_stocks_with_increased_volume
 from report_sheet import update_report
-from config import *
+
+
+SCOPE = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive.file',
+         'https://www.googleapis.com/auth/drive']
+GOOGLE_CREDS = r'/home/albi/projects/stock_market_insights/stock_market_insights/gspreadreporter/secret/credentials.json'
+SPREADSHEET_ID = '1FwrZ30o_JZEKN7ANwwnuc7ZiUxxdjWQ6pfEbcamlaZ4'
+
+WS_REPORT = 'Raport'
+WS_WATCHED = 'Obserwowane'
+
+WIG20 = [
+    'ALE', 'ALR', 'CCC', 'CDR', 'CPS',
+    'DNP', 'JSW', 'KGH', 'LPP', 'LTS',
+    'OPL', 'PEO', 'PGE', 'PGN', 'PKN',
+    'PKO', 'PLY', 'PZU', 'SPL', 'TPE'
+]
+
+MWIG40 = [
+    '11B', 'ACP', 'AMC', 'ASE', 'ATT', 'BDX', 'BFT', 'BHW', 'BNP', 'CAR',
+    'CIE', 'CLN', 'CMR', 'DOM', 'DVL', 'EAT', 'ECH', 'ENA', 'ENG', 'EUR',
+    'FMF', 'FTE', 'GPW', 'GTC', 'GTN', 'ING', 'KER', 'KRU', 'KTY', 'LVC',
+    'LWB', 'MAB', 'MIL', 'NEU', 'PKP', 'PLW', 'STP', 'TEN', 'VRG', 'WPL'
+]
+
+MY_TICKERS = WIG20 + MWIG40
 
 
 store_data()
 
 
-def select_stocks_with_increased_volume(stocks_dict: dict, long=90, factor=3.3) -> dict:
-    selected = dict()
-
-    for tck in stocks_dict.keys():
-        stock = stocks_dict[tck]
-        long_mean = stock.mean_volume(long)
-        if (stock.volume/long_mean) > factor:
-            selected[tck] = stock
-
-    return selected
-
-
 def update_stocks_spreadsheet(spreadsheet):
-    # wallet = spreadsheet.worksheet(WS_WALLET)
     report = spreadsheet.worksheet(WS_REPORT)
-    watched = spreadsheet.worksheet(WS_WATCHED)
-
-    """ UPDATE WALLET WORKSHEET """
-    # update_wallet(wallet)
 
     """ GET INCREASED VOLUME STOCKS """
     stocks_to_analyse = {s: Stock(s) for s in MY_TICKERS}
@@ -38,11 +48,7 @@ def update_stocks_spreadsheet(spreadsheet):
     print(f'{len(selected_stocks)} stocks selected')
 
     """ UPDATE ANALYSIS REPORT """
-    print('Adding watched stocks...')
-    watched_stocks = watched.col_values(TICKERS_COL)[FIRST_DATA_ROW - 1::]
-    report_stocks = {s: Stock(s) for s in watched_stocks}
-    report_stocks.update(selected_stocks)
-    update_report(report, report_stocks)
+    update_report(report, selected_stocks)
 
 
 if __name__ == '__main__':
